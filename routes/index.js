f29811fcdfb2bea201866384be9ccfacf2d70eef
Ryosuke Goto <ryosuke@direcsio.jp>
2016-02-14 18:56:52 +0900

    packageの依存関係のエラーを解消
    - mochaでtestフォルダ内unit testが実行できるように
    - mongodbをinstall

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
+router.get('/test', function (req, res) {
+    res.send('Test it!');
+});
 module.exports = router;
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,6 @@
 /// <reference path="mime/mime.d.ts" />
 /// <reference path="node/node.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="superagent/superagent.d.ts" />
